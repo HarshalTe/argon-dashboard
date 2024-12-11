e047c0e0566978c88436aa58f2c7cc399db70b61
HarshalTe <harshalterekar509@gmail.com>
2024-12-11 15:58:04 +0530

    -initilize the argon dashboard

---
+++ src/views/examples/Account.js
@@ -0,0 +1,14 @@
+import Header from "components/Headers/Header";
+
+const Account = ()=>{
+    return(
+        <>
+                <Header />
+               
+                   <h1>my page</h1>
+           
+        
+        </>
+    );
+}
+export default Account;
